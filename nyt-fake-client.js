452b5479a4e381c7cf3975f76b72f8373dacb86e
Billy Taggart <williamtaggart97@gmail.com>
2022-03-18 10:27:41 -0400

    added option for command line apiKey

--- nyt-fake-client.js
+++ nyt-fake-client.js
@@ -1,6 +1,17 @@
 import { NytIntegrationService } from "./nyt-integration.service.js";
 
 const nytApiKey = '80xA0tBragGQqDxOt2EMePRw2ZkIpQ1W';
+// let nytApiKey;
+// const keyFlagPosition = process.argv.indexOf('-k');
+// if (keyFlagPosition > -1) {
+//     nytApiKey = process.argv[keyFlagPosition + 1];
+//     if (!nytApiKey) {
+//         throw new Error('api key must be placed directly after -k flag');
+//     }
+// } else {
+//     throw new Error('api key not found: missing -k flag')
+// }
+
 const nytIntegrationService = new NytIntegrationService(nytApiKey);
 
 
